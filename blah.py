0aa9ed2c98d6970272b5bcdb7d2bd2d109f0490d
DataMonarch <toghrultahirov2@gmail.com>
2022-01-03 10:59:01 +0400

    Simple Graph

--- blah.py
+++ blah.py
@@ -3,7 +3,12 @@ import matplotlib.pyplot as plt
 
 x = np.random.normal(10, 5, 1000)
 y = np.random.normal(0, 1, 1000)
-
+xx, yy = np.meshgrid(x, y)
+z = xx * yy
+z.shape
 plt.plot(y, x)
 
 plt.plot(x, y)
+
+plt.hist(z.flatten())
+plt.imshow(z)
